9b50cf415478a6b4431a788d8e48d11d372be70b
Motta <abdelmoutalib.nioua@le-campus-numerique.fr>
2024-07-09 15:44:51 +0200

    last modification about operator overloading

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from serpent import serpent
 from zoo import zoo
 
 ###### Main #####
+#creer un environement conda
+#conda create --name $ENVIRONMENT_NAME python tensorflow
 
 if __name__=="__main__":
      
     A1=Animal(18,15)
     S1=serpent(1,2)
     O1=Oiseaux(100,200,300)
-    Z=zoo([O1,S1])
+    Z1=zoo([O1,S1])
+    Z2=zoo([O1,S1,O1])
 
     print("size=",A1.get_size())
     print("wieght=",A1.get_weight())
@@ -22,9 +25,11 @@ if __name__=="__main__":
     print("wieght=",O1.get_weight())
     print("altitude=",O1.get_altitude_max())
     #O1.set_size(-2)
-    Z.add_animal(S1)
-    Z.show_an()
-    
+    Z1.add_animal(S1)
+    Z1.show_an()
+    Z3 = Z1 + Z2
+    print(type(Z3))
+    Z3.show_an()
     
 
 
--- zoo.py
+++ zoo.py
@@ -11,4 +11,14 @@ class zoo:
         for ani in self.animals:
             print(type(ani))
             ani.__str__(ani)
+
+    def __add__(self, other):
+        self.animals+=other.animals
+        return zoo(self.animals) 
+    
+
+    def add_zoo(self,Z1,Z2):
+       Z1.animals += Z2.animals
+       return Z1 
+
              
